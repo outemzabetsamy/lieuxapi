9f6ad83723d7102f8611e219f83181e40198c2f3
outemzabetsamy <44290778+outemzabetsamy@users.noreply.github.com>
2021-04-01 21:23:41 +0100

    Update 20210312132131_InitialMigratio.cs

--- RestApiSelami/Migrations/20210312132131_InitialMigratio.cs
+++ RestApiSelami/Migrations/20210312132131_InitialMigratio.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace RestApiSelami.Migrations
 {
-    public partial class InitialMigration : Migration
+   public partial class InitialMigratio : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
